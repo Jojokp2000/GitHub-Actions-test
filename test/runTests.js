ea17db797687a941ad92cbbd189e0d0a96386c05
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-03-25 21:09:59 +0100

    Update runTests.js

--- test/runTests.js
+++ test/runTests.js
@@ -1,1 +1,10 @@
-console.log('test');
+async function runTests() {
+
+  const options = { root: [__dirname] }
+
+  const testResults = await jest.runCLI(options, options.root);
+  
+  console.log(testResults);
+}
+
+runTests();
