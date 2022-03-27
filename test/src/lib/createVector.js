583529434ce24bf1f8d50e16704c908ee21bb211
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-03-27 21:24:50 +0200

    testing commit

--- test/src/lib/createVector.js
+++ test/src/lib/createVector.js
@@ -1,4 +1,6 @@
-const createVector = require('src/lib/createVector.js');test('Create 3D vector', async () => {
+const createVector = require('src/lib/createVector.js');
+
+test('Create 3D vector', async () => {
     const result = await createVector(1,2,3);
     expect(result).toEqual({ x: 1, y: 2, z: 3 });
 })
