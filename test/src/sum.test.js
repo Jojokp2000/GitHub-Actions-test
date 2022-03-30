67f81caedad600defcafd37427086f8e6e1c30e6
Thorbjørn Møller <66469783+thor1878@users.noreply.github.com>
2022-03-30 14:35:50 +0200

    testing commit

--- test/src/sum.test.js
+++ test/src/sum.test.js
@@ -1,6 +1,6 @@
 const sum = require('src/sum.js');
 
 test('Sum two numbers', async () => {
-    const result = await sum(4,6);
-    expect(result).toBe(11);
+    const result = await sum(3,4);
+    expect(result).toBe(7);
 })
