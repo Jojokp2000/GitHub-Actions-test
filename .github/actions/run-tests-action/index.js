044aa55c8d0919f36afd00127f115d8e1134e508
Johannes Karstoft Pedersen <93575274+Jojokp2000@users.noreply.github.com>
2022-04-24 18:04:35 +0200

    testing commit

--- .github/actions/run-tests-action/index.js
+++ .github/actions/run-tests-action/index.js
@@ -1,15 +1,14 @@
 const core = require('@actions/core');
 const jest = require('jest');
 
-const options = { root: [__dirname] }
+const options = { 
+    root: [__dirname], // ?????
+};
 
 jest.runCLI(options, options.root)
 .then(results => {
-    if (results.results.success) {
-        core.setOutput('results', results);
-    } else {
-        core.setFailed('At least 1 test failed.');
-    }
+    core.setOutput('results', results);
 })
-
-
+.catch(failure => {
+    console.log(failure)
+})
--- .github/actions/wait-for-response-action/index.js
+++ .github/actions/wait-for-response-action/index.js
@@ -1,22 +1,56 @@
 const core = require('@actions/core');
 const fetch = require('node-fetch');
+const config = require('../../config.json');
 
-const url = 'https://cc3a-130-225-198-131.ngrok.io/test';
+const repo = core.getInput('repo');
+const branch = core.getInput('branch');
+const pollInterval = 5000;
 
-let seconds = 0;
+console.log(repo);
+console.log(branch);
 
-const interval = setInterval(() => {
+const url = config.tsURL;
 
-    fetch(url)
-    .then(response => response.status)
-    .then(status => {
-        console.log(status);
-        if (status === 200) {
-            clearInterval(interval);
-        }
+// Keep polling test server until receiving a 200
+poll();
+
+async function poll() {
+    ready = false;
+
+    while(!ready) {
+        ready = await getStatus();
+        await wait(pollInterval);
+    }
+}
+
+async function wait(timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, timeout);
+    })
+}
+
+async function getStatus() {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            origin: "GA",
+            repository: repo,
+            branch: branch
+        })
     })
 
-    console.log(seconds + ' seconds');
-    seconds++;
-
-}, 1000)
+    const status = response.status;
+    
+    if (status === 200) {
+        const data = await response.json();
+        core.setOutput("user-test-info", data);
+        return true;
+    } else {
+        return false;
+    }
+}
